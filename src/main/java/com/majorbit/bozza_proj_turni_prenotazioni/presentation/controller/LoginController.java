78bffd30da008bc63e5bd26a8da2191a6f1a4e42
IvanStante95 <ivanstante8@gmail.com>
2024-06-10 16:44:21 +0200

    controller

---
+++ src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/presentation/controller/LoginController.java
@@ -0,0 +1,54 @@
+package com.majorbit.bozza_proj_turni_prenotazioni.presentation.controller;
+
+import com.majorbit.bozza_proj_turni_prenotazioni.application.dto.LoginDTO;
+import com.majorbit.bozza_proj_turni_prenotazioni.application.service.impl.ILoginService;
+import com.majorbit.bozza_proj_turni_prenotazioni.application.service.spec.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/login")
+public class LoginController {
+
+    private final ILoginService loginService;
+
+    @Autowired
+    public LoginController(ILoginService loginService) {
+        this.loginService = loginService;
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<LoginDTO> getLoginById(@PathVariable Long id) {
+        LoginDTO loginDTO = loginService.getLoginById(id);
+        return ResponseEntity.ok(loginDTO);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LoginDTO>> getAllLogins() {
+        List<LoginDTO> loginDTOs = loginService.getAllLogins();
+        return ResponseEntity.ok(loginDTOs);
+    }
+
+    @PostMapping
+    public ResponseEntity<LoginDTO> createLogin(@RequestBody LoginDTO loginDTO) {
+        LoginDTO createdLoginDTO = loginService.createLogin(loginDTO);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdLoginDTO);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<LoginDTO> updateLogin(@PathVariable Long id, @RequestBody LoginDTO loginDTO) {
+        LoginDTO updatedLoginDTO = loginService.updateLogin(id, loginDTO);
+        return ResponseEntity.ok(updatedLoginDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteLogin(@PathVariable Long id) {
+        loginService.deleteLogin(id);
+        return ResponseEntity.noContent().build();
+    }
+}
