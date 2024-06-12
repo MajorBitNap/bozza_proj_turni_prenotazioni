8ebd1a37d44a7423a1b9d6afab3f52283aedc552
edoardoantola <e.antola@majorbit.com>
2024-06-12 10:43:23 +0200

    aggiunta classi vuote per sec e jwt

---
+++ src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/application/dto/LoginRequestDTO.java
@@ -0,0 +1,2 @@
+package com.majorbit.bozza_proj_turni_prenotazioni.application.dto;public class LoginRequestDTO {
+}
---
+++ src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/presentation/controller/AuthController.java
@@ -0,0 +1,2 @@
+package com.majorbit.bozza_proj_turni_prenotazioni.presentation.controller;public class AuthController {
+}
