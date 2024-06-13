4aa3f701f7462979e7849e380ebe3fb57bf73bb4
edoardoantola <e.antola@majorbit.com>
2024-06-13 11:52:05 +0200

    prova test

--- src/test/java/com/majorbit/bozza_proj_turni_prenotazioni/BozzaProjTurniPrenotazioniApplicationTests.java
+++ src/test/java/com/majorbit/bozza_proj_turni_prenotazioni/BozzaProjTurniPrenotazioniApplicationTests.java
@@ -3,31 +3,9 @@ package com.majorbit.bozza_proj_turni_prenotazioni;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
 @SpringBootTest
 class BozzaProjTurniPrenotazioniApplicationTests {
-
-	Calculator underTest = new Calculator();
-
 	@Test
 	void itShouldAddNumbers() {
-
-		//given
-		var numberOne = 20;
-		var numberoTwo = 30;
-
-		//when
-		var result = underTest.add(numberOne, numberoTwo);
-
-		//then
-		assertThat(result).isEqualTo(50);
-
-	}
-
-	class Calculator {
-		int add(int a, int b) {
-			return a +b;
-		}
 	}
 }
