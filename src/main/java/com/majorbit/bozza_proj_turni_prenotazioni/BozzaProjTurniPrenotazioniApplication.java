22fa243bf26995d3fc5493701f7089f6616426fc
edoardoantola <e.antola@majorbit.com>
2024-06-14 10:41:27 +0200

    prova test

--- src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/BozzaProjTurniPrenotazioniApplication.java
+++ src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/BozzaProjTurniPrenotazioniApplication.java
@@ -5,14 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.Arrays;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.majorbit.bozza_proj_turni_prenotazioni")
 public class BozzaProjTurniPrenotazioniApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext app = SpringApplication.run(BozzaProjTurniPrenotazioniApplication.class, args);
+		SpringApplication.run(BozzaProjTurniPrenotazioniApplication.class, args);
 	}
 
 }
