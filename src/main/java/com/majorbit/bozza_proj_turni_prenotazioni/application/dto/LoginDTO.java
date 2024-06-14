0c605655a4d3759d69d337999ce935bb4eecfa49
edoardoantola <e.antola@majorbit.com>
2024-06-14 11:38:05 +0200

    prova test

--- src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/application/dto/LoginDTO.java
+++ src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/application/dto/LoginDTO.java
@@ -1,7 +1,7 @@
 package com.majorbit.bozza_proj_turni_prenotazioni.application.dto;
 
 import com.majorbit.bozza_proj_turni_prenotazioni.domain.model.Utente;
-
+//implementa serializable
 public record LoginDTO(
         Long id,
         Utente utente,
