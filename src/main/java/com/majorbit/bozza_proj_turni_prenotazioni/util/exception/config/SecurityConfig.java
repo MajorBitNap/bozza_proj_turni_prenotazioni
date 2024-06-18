a2672fc765daadafdba928b6bd5be700fba6994d
edoardoantola <e.antola@majorbit.com>
2024-06-18 17:22:27 +0200

    aggiunta classe config sicurezza che permette ogni richiesta senza alcun tipo tdi autorizzazione, per effetture prove

---
+++ src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/util/exception/config/SecurityConfig.java
@@ -0,0 +1,24 @@
+package com.majorbit.bozza_proj_turni_prenotazioni.util.exception.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(authorize -> authorize
+                        .anyRequest().permitAll()  // Permette tutte le richieste senza autenticazione
+                )
+                .csrf(csrf -> csrf.disable());  // Disabilita CSRF
+
+        return http.build();
+    }
+
+}
+
+
