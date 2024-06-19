8f594033100d815577f2063ea8205225647e15ad
edoardoantola <e.antola@majorbit.com>
2024-06-19 17:41:00 +0200

    logica id per relazione DTO

--- src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/application/dto/PrenotazioneDTO.java
+++ src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/application/dto/PrenotazioneDTO.java
@@ -4,12 +4,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.sql.Date;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class PrenotazioneDTO implements Serializable {
+
+    @Serial
     private static final long serialVersionUID = 1L;
 
     private Date dataInizio;
