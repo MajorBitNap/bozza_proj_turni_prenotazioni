6df677cee19a041080c079923265cf37fbf0c8d9
edoardoantola <e.antola@majorbit.com>
2024-06-21 14:40:11 +0200

    rimozione UserDetail per chiarezza branch

--- src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/domain/model/Utente.java
+++ src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/domain/model/Utente.java
@@ -15,7 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-public class Utente implements UserDetails {
+public class Utente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,38 +36,4 @@ public class Utente implements UserDetails {
     @Column(nullable = false)
     private Ruolo ruolo;
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(ruolo.name()));
-    }
-
-    @Override
-    public String getPassword() {
-        return "";
-    }
-
-    @Override
-    public String getUsername() {
-        return email;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-            return true;
-    }
 }
