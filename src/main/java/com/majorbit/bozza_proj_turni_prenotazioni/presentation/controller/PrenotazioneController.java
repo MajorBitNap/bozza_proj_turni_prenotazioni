6e252dc2b4a6fc87e5798a633a4af826cebe4dfd
edoardoantola <e.antola@majorbit.com>
2024-06-25 09:31:09 +0200

    utilizzo riferimento metodo come suggerimento di intellij

--- src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/presentation/controller/PrenotazioneController.java
+++ src/main/java/com/majorbit/bozza_proj_turni_prenotazioni/presentation/controller/PrenotazioneController.java
@@ -6,6 +6,7 @@ import com.majorbit.bozza_proj_turni_prenotazioni.application.usecases.impl.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -51,11 +52,13 @@ public class PrenotazioneController {
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
+
     @PostMapping("/moderatore")
     public ResponseEntity<PrenotazioneDTO> modCreaPrenotazione(@RequestBody PrenotazioneDTO PrenotazioneDTO) {
         var newPrenotazione = modCreaPrenotazione.createPrenotazione(PrenotazioneDTO);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
     @PutMapping("/approvazione/{id}")
     public ResponseEntity<PrenotazioneDTO> approvaPrenotazione(@PathVariable Integer id) {
         var approvedPrenotazione = approvaPrenotazione.approvaPrenotazione(id);
